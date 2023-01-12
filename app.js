05f827cd4c37f3e4ff8752857925910799da938f
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-01-13 02:16:39 +0530

    premiumuser_task

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var cors = require('cors')
 const sequelize = require('./util/database');
 const User = require('./models/users');
 const Expense = require('./models/expenses');
-
+const Order = require('./models/orders');
 
 const userRoutes = require('./routes/user')
 const expenseRoutes = require('./routes/expense')
+const purchaseRoutes = require('./routes/purchase')
 
 const app = express();
 const dotenv = require('dotenv');
@@ -22,13 +23,15 @@ app.use(cors());
 // app.use(bodyParser.urlencoded());  ////this is for handling forms
 app.use(express.json());  //this is for handling jsons
 
-app.use('/user', userRoutes)
-app.use('/expense', expenseRoutes)
+app.use('/user', userRoutes);
+app.use('/expense', expenseRoutes);
+app.use('/purchase', purchaseRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
-
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync()
     .then(() => {
